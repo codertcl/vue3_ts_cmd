181f0106b93270e3ebacec2962754278ea9afbbc
wuxinabc <45070627+wuxinabc@users.noreply.github.com>
2022-06-18 15:09:58 +0800

    test: 测试commitlint/config-conventional

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <h3>测试commitlint/config-conventional</h3>
 </template>
 
 <script lang="ts">
